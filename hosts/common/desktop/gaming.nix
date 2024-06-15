4689144b60b208eb04f1b96b1ee0f6d56972682d
Nyxerproject <nxyerproject@gmail.com>
2024-06-14 23:43:26 -0500

    25 current  2024-06-14 23:09:43  24.11.20240613.e9ee548   6.6.33                          *

---
+++ hosts/common/desktop/gaming.nix
@@ -0,0 +1,54 @@
+{
+  config,
+  inputs,
+  pkgs,
+  ...
+}: {
+  nixpkgs.overlays = [
+    inputs.prismlauncher.overlays.default
+  ];
+
+  environment.systemPackages = with pkgs; [
+    #dolphin-emu
+    #prismlauncher
+    r2modman
+    #ryujinx
+
+    protontricks
+  ];
+
+  programs.steam = {
+    enable = true;
+    localNetworkGameTransfers.openFirewall = true;
+    remotePlay.openFirewall = true;
+
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+      (proton-ge-bin.overrideAttrs (finalAttrs: _: {
+        version = "GE-Proton9-4-rtsp7";
+        src = pkgs.fetchzip {
+          url = "https://github.com/SpookySkeletons/proton-ge-rtsp/releases/download/${finalAttrs.version}/${finalAttrs.version}.tar.gz";
+          hash = "sha256-l/zt/Kv6g1ZrAzcxDNENByHfUp/fce3jOHVAORc5oy0=";
+        };
+      }))
+    ];
+  };
+
+  programs.gamemode = {
+    enable = true;
+    settings = {
+      general = {
+        defaultgov = config.powerManagement.cpuFreqGovernor;
+        desiredgov = "performance";
+        softrealtime = "on";
+        renice = 10;
+        ioprio = 1;
+        inhibit_screensaver = 0;
+      };
+      custom = {
+        start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
+        stop = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
+      };
+    };
+  };
+}
