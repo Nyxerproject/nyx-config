f0ffebadee845d4922c36339e4fbd762b9171f91
Nyxerproject <nxyerproject@gmail.com>
2024-06-14 21:45:48 -0500

    20 current  2024-06-14 21:40:31  24.11.20240613.e9ee548   6.6.33                          *

--- hosts/common/desktop/vr/default.nix
+++ hosts/common/desktop/vr/default.nix
@@ -20,45 +20,45 @@
   #  inputs.nixpkgs-xr.nixosModules.nixpkgs-xr
   #];
 
-  config = {
-    #nixpkgs.xr.enableUnstripped = true;
+  #config = {
+  #nixpkgs.xr.enableUnstripped = true;
 
-    #boot.extraModulePackages = mkIf cfg.enableHighPrioKernelPatch [
-    #(amdgpu-kernel-module.overrideAttrs (prev: {
-    #patches = (prev.patches or []) ++ [inputs.scrumpkgs.kernelPatches.cap_sys_nice_begone.patch];
-    #}))
-    #];
+  #boot.extraModulePackages = mkIf cfg.enableHighPrioKernelPatch [
+  #(amdgpu-kernel-module.overrideAttrs (prev: {
+  #patches = (prev.patches or []) ++ [inputs.scrumpkgs.kernelPatches.cap_sys_nice_begone.patch];
+  #}))
+  #];
 
-    services.monado = {
-      enable = true;
-      defaultRuntime = true;
-    };
-
-    environment.systemPackages = with pkgs; [
-      index_camera_passthrough
-      opencomposite-helper
-      wlx-overlay-s
-    ];
-
-    hm.xdg.configFile."openvr/openvrpaths.vrpath".text = ''
-      {
-        "config" :
-        [
-          "${config.hm.xdg.dataHome}/Steam/config"
-        ],
-        "external_drivers" : null,
-        "jsonid" : "vrpathreg",
-        "log" :
-        [
-          "${config.hm.xdg.dataHome}/Steam/logs"
-        ],
-        "runtime" :
-        [
-          "${pkgs.opencomposite}/lib/opencomposite",
-          "${config.hm.xdg.dataHome}/Steam/steamapps/common/SteamVR"
-        ],
-        "version" : 1
-      }
-    '';
+  services.monado = {
+    enable = true;
+    defaultRuntime = true;
   };
+
+  environment.systemPackages = with pkgs; [
+    index_camera_passthrough
+    opencomposite-helper
+    wlx-overlay-s
+  ];
+
+  hm.xdg.configFile."openvr/openvrpaths.vrpath".text = ''
+    {
+      "config" :
+      [
+        "${config.hm.xdg.dataHome}/Steam/config"
+      ],
+      "external_drivers" : null,
+      "jsonid" : "vrpathreg",
+      "log" :
+      [
+        "${config.hm.xdg.dataHome}/Steam/logs"
+      ],
+      "runtime" :
+      [
+        "${pkgs.opencomposite}/lib/opencomposite",
+        "${config.hm.xdg.dataHome}/Steam/steamapps/common/SteamVR"
+      ],
+      "version" : 1
+    }
+  '';
+  #};
 }
