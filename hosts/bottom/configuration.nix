659d1b02834d33b77940777381947dbbe9167a5c
Nyxerproject <nxyerproject@gmail.com>
2024-06-15 20:26:39 -0500

    30 current  2024-06-15 20:26:37  24.11.20240613.e9ee548   6.6.33                          *

--- hosts/bottom/configuration.nix
+++ hosts/bottom/configuration.nix
@@ -25,6 +25,11 @@
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
 
+  boot.extraModprobeConfig = ''
+    # Fix Nintendo Switch Pro Controller disconnects
+    options bluetooth disable_ertm=1
+  '';
+
   # Set your time zone.
   time.timeZone = "America/Chicago";
 
