ef87ec6f7ced9e75dac5ca1525496d01a47ad86c
Nyxerproject <nxyerproject@gmail.com>
2024-06-20 20:34:09 -0500

    42 current  2024-06-20 20:34:07  24.11.20240613.e9ee548  6.9.3-zen1                          *

--- hosts/common/desktop/vr/default.nix
+++ hosts/common/desktop/vr/default.nix
@@ -5,25 +5,26 @@
   pkgs,
   ...
 }: let
-  inherit (lib.modules) mkIf;
-  inherit (lib.options) mkEnableOption;
-  cfg = config.profile.vr;
-  amdgpu-kernel-module = pkgs.callPackage ./amdgpu.nix {
-    kernel = config.boot.kernelPackages.kernel;
-  };
+  # inherit (lib.modules) mkIf;
+  # inherit (lib.options) mkEnableOption;
+  #cfg = config.profile.vr;
+  # amdgpu-kernel-module = pkgs.callPackage ./amdgpu.nix {
+  # kernel = config.boot.kernelPackages.kernel;
+  # };
 in {
-  options.profile.vr.enableHighPrioKernelPatch = mkEnableOption "kernel patch to allow high priority graphics for all clients";
+  # options.profile.vr.enableHighPrioKernelPatch = mkEnableOption "kernel patch to allow high priority graphics for all clients";
 
   config = {
-    nixpkgs.xr.enableUnstripped = true;
+    # nixpkgs.xr.enableUnstripped = true;
 
-    boot.extraModulePackages = mkIf cfg.enableHighPrioKernelPatch [
-      (amdgpu-kernel-module.overrideAttrs (prev: {
-        patches = (prev.patches or []) ++ [inputs.scrumpkgs.kernelPatches.cap_sys_nice_begone.patch];
-      }))
-    ];
+    #boot.extraModulePackages = mkIf cfg.enableHighPrioKernelPatch [
+    # (amdgpu-kernel-module.overrideAttrs (prev: {
+    # patches = (prev.patches or []) ++ [inputs.scrumpkgs.kernelPatches.cap_sys_nice_begone.patch];
+    # }))
+    # ];
 
     systemd.user.services.monado.environment = {
+      # systemd.nyx.services.monado.environment = {
       STEAMVR_LH_ENABLE = "1";
       XRT_COMPOSITOR_COMPUTE = "1";
       WMR_HANDTRACKING = "0";
