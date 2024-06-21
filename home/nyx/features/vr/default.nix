28fdfddc8868698cfe46cf436903230d58e2f0a3
Nyxerproject <nxyerproject@gmail.com>
2024-06-20 23:05:55 -0500

    50 current  2024-06-20 23:05:52  24.11.20240618.c00d587  6.9.3-zen1                          *

--- home/nyx/features/vr/default.nix
+++ home/nyx/features/vr/default.nix
@@ -5,16 +5,6 @@
   pkgs,
   ...
 }: {
-  xdg.configFile."openxr/1/active_runtime.json".text = ''
-    {
-      "file_format_version": "1.0.0",
-      "runtime": {
-          "name": "Monado",
-          "library_path": "${pkgs.monado}/lib/libopenxr_monado.so"
-      }
-    }
-  '';
-
   xdg.configFile."openvr/openvrpaths.vrpath".text = ''
     {
       "config" :
