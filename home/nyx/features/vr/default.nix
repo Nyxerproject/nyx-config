05a0d591c1395b61760c9e56742f17d08178e56a
Nyxerproject <nxyerproject@gmail.com>
2024-06-22 22:54:13 -0500

    60 current  2024-06-22 22:54:11  24.11.20240618.c00d587  6.9.3-zen1                          *

--- home/nyx/features/vr/default.nix
+++ home/nyx/features/vr/default.nix
@@ -15,24 +15,24 @@
   # }
   # '';
 
-  xdg.configFile."openvr/openvrpaths.vrpath".text = ''
-    {
-      "config" :
-      [
-        "${config.xdg.dataHome}/Steam/config"
-      ],
-      "external_drivers" : null,
-      "jsonid" : "vrpathreg",
-      "log" :
-      [
-        "${config.xdg.dataHome}/Steam/logs"
-      ],
-      "runtime" :
-      [
-        "${pkgs.opencomposite}/lib/opencomposite",
-        "${config.xdg.dataHome}/Steam/steamapps/common/SteamVR"
-      ],
-      "version" : 1
-    }
-  '';
+  # xdg.configFile."openvr/openvrpaths.vrpath".text = ''
+  # {
+  # "config" :
+  # [
+  # "${config.xdg.dataHome}/Steam/config"
+  # ],
+  # "external_drivers" : null,
+  # "jsonid" : "vrpathreg",
+  # "log" :
+  # [
+  # "${config.xdg.dataHome}/Steam/logs"
+  # ],
+  # "runtime" :
+  # [
+  # "${pkgs.opencomposite}/lib/opencomposite",
+  # "${config.xdg.dataHome}/Steam/steamapps/common/SteamVR"
+  # ],
+  # "version" : 1
+  # }
+  # '';
 }
