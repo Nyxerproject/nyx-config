4f65b7706ea87682142e2d37af6e2bc1fd2f7308
Nyxerproject <nxyerproject@gmail.com>
2024-06-23 22:04:33 -0500

    78 current  2024-06-23 22:04:30  24.11.20240618.c00d587  6.9.3-zen1                          *

--- home/nyx/features/vr/default.nix
+++ home/nyx/features/vr/default.nix
@@ -5,34 +5,34 @@
   pkgs,
   ...
 }: {
-  xdg.configFile."openxr/1/active_runtime.json".text = ''
-    {
-      "file_format_version": "1.0.0",
-      "runtime": {
-          "name": "Monado",
-          "library_path": "${pkgs.monado}/lib/libopenxr_monado.so"
-      }
-    }
-  '';
+  # xdg.configFile."openxr/1/active_runtime.json".text = ''
+  # {
+  # "file_format_version": "1.0.0",
+  # "runtime": {
+  # "name": "Monado",
+  # "library_path": "${pkgs.monado}/lib/libopenxr_monado.so"
+  # }
+  # }
+  # '';
 
-  xdg.configFile."openvr/openvrpaths.vrpath".text = ''
-    {
-      "config" :
-      [
-        "${config.xdg.dataHome}/Steam/config"
-      ],
-      "external_drivers" : null,
-      "jsonid" : "vrpathreg",
-      "log" :
-      [
-        "${config.xdg.dataHome}/Steam/logs"
-      ],
-      "runtime" :
-      [
-        "${pkgs.opencomposite}/lib/opencomposite",
-        "${config.xdg.dataHome}/Steam/steamapps/common/SteamVR"
-      ],
-      "version" : 1
-    }
-  '';
+  # xdg.configFile."openvr/openvrpaths.vrpath".text = ''
+  # {
+  # "config" :
+  # [
+  # "${config.xdg.dataHome}/Steam/config"
+  # ],
+  # "external_drivers" : null,
+  # "jsonid" : "vrpathreg",
+  # "log" :
+  # [
+  # "${config.xdg.dataHome}/Steam/logs"
+  # ],
+  # "runtime" :
+  # [
+  # "${pkgs.opencomposite}/lib/opencomposite",
+  # "${config.xdg.dataHome}/Steam/steamapps/common/SteamVR"
+  # ],
+  # "version" : 1
+  # }
+  # '';
 }
