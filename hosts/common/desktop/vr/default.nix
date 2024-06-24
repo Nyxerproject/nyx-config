4ee0aa5e233446f10b8001f39a041c9be5019fb0
Nyxerproject <nxyerproject@gmail.com>
2024-06-23 20:31:09 -0500

    75 current  2024-06-23 20:31:06  24.11.20240618.c00d587  6.9.3-zen1                          *

--- hosts/common/desktop/vr/default.nix
+++ hosts/common/desktop/vr/default.nix
@@ -10,13 +10,13 @@
   config = {
     nixpkgs.xr.enableUnstripped = true;
 
-    environment.variables = {
-      # systemd.user.services.monado.environment = {
-      STEAMVR_LH_ENABLE = "1";
-      XRT_COMPOSITOR_COMPUTE = "1";
-      WMR_HANDTRACKING = "0";
-      # XRT_HAVE_STEAM = "YES";
-    };
+    # environment.variables = {
+    # systemd.user.services.monado.environment = {
+    # STEAMVR_LH_ENABLE = "1";
+    # XRT_COMPOSITOR_COMPUTE = "1";
+    # WMR_HANDTRACKING = "0";
+    # XRT_HAVE_STEAM = "YES";
+    # };
 
     services.monado = {
       enable = true;
