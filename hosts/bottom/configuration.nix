ae2229d55e0ec48205bc2db2eff9bfdda230083e
Nyxerproject <nxyerproject@gmail.com>
2024-07-01 22:21:50 -0500

    79 current  2024-07-01 22:21:48  24.11.20240618.c00d587  6.9.3-zen1                          *

--- hosts/bottom/configuration.nix
+++ hosts/bottom/configuration.nix
@@ -131,6 +131,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    zlib
     lunarvim
     alacritty
     wget
