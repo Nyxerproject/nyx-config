006b4c4528f492c2ddc959ce80f0c631d9753728
Nyxerproject <nxyerproject@gmail.com>
2024-07-05 00:47:22 -0500

    104 current  2024-07-05 00:47:19  24.11.20240703.9f4128e  6.9.7-zen1                          *

--- hosts/common/desktop/vr/default.nix
+++ hosts/common/desktop/vr/default.nix
@@ -17,12 +17,16 @@
   # WMR_HANDTRACKING = "0";
   # XRT_HAVE_STEAM = "YES";
   # };
+  # environment.variables = {
   systemd.user.services.monado.environment = {
     STEAMVR_LH_ENABLE = "1";
     XRT_COMPOSITOR_COMPUTE = "1";
     WMR_HANDTRACKING = "0";
   };
 
+  environment.variables = {
+    LIBMONADO_PATH = "${pkgs.monado}";
+  };
   services.monado = {
     enable = true;
     defaultRuntime = true;
