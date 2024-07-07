e816130f3687b2cf9cd55d45d5923f97a6944230
Nyxerproject <nxyerproject@gmail.com>
2024-07-07 02:34:04 -0500

    120 current  2024-07-07 02:34:01  24.11.20240703.9f4128e  6.9.7-zen1                          *

--- hosts/bottom/configuration.nix
+++ hosts/bottom/configuration.nix
@@ -75,6 +75,7 @@ in {
 
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
+  zramSwap.enable = true;
 
   boot.extraModprobeConfig = ''
     # Fix Nintendo Switch Pro Controller disconnects
