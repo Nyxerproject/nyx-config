7a8a1ad4543fb76d2e74099be23cd98edabbf730
Nyxerproject <nxyerproject@gmail.com>
2024-07-07 22:31:39 -0500

    125 current  2024-07-07 22:31:35  24.11.20240703.9f4128e  6.9.7-zen1                          *

--- hosts/bottom/configuration.nix
+++ hosts/bottom/configuration.nix
@@ -2,60 +2,50 @@
   config,
   inputs,
   pkgsmndvlknlyrs,
-  system ? builtins.currentSystem,
+  system,
   pkgs,
   ...
-}: let
-  nyx-rebuild =
-    pkgs.writers.writeBashBin "nyx-rebuild" {}
-    /*
-    bash
-    */
-    ''
-      #!/usr/bin/env bash
-
-      # A rebuild script that commits on a successful build
-      set -e
-
-      # Edit your config
-      $EDITOR ~/nyx-config/
-
-      # cd to your config dir
-      pushd ~/nyx-config/
-
-      # Early return if no changes were detected (thanks @singiamtel!)
-      #if git diff --cached --quiet HEAD -- '*.nix'; then
-      if git diff --quiet HEAD -- '*.nix'; then
-          echo "No changes detected, exiting."
-          popd
-          exit 0
-      fi
-
-      # Autoformat your nix files
-      alejandra . &>/dev/null \
-        || ( alejandra . ; echo "formatting failed!" && exit 1)
-
-      # Shows your changes
-      git diff -U0 '*.nix'
-
-      echo "NixOS Rebuilding..."
-
-      # Rebuild, output simplified errors, log trackebacks
-      sudo nixos-rebuild switch --flake .#bottom &>nixos-switch.log || (cat nixos-switch.log | grep --color error && exit 1)
-
-      # Get current generation metadata
-      current=$(nixos-rebuild list-generations | grep current)
-
-      # Commit all changes witih the generation metadata
-      git commit -am "$current"
-
-      # Back to where you were
-      popd
-
-      # Notify all OK!
-      notify-send -e "NixOS Rebuilt OK!" --icon=software-update-available
-    '';
-in {
+}:
+# let
+#   nyx-rebuild =
+#     pkgs.writers.writeBashBin "nyx-rebuild" {}
+#     /*
+#     bash
+#     */
+#     ''
+#       #!/usr/bin/env bash
+#       # A rebuild script that commits on a successful build
+#       set -e
+#       # Edit your config
+#       $EDITOR ~/nyx-config/
+#       # cd to your config dir
+#       pushd ~/nyx-config/
+#       # Early return if no changes were detected (thanks @singiamtel!)
+#       #if git diff --cached --quiet HEAD -- '*.nix'; then
+#       if git diff --quiet HEAD -- '*.nix'; then
+#           echo "No changes detected, exiting."
+#           popd
+#           exit 0
+#       fi
+#       # Autoformat your nix files
+#       alejandra . &>/dev/null \
+#         || ( alejandra . ; echo "formatting failed!" && exit 1)
+#       # Shows your changes
+#       git diff -U0 '*.nix'
+#       echo "NixOS Rebuilding..."
+#       # Rebuild, output simplified errors, log trackebacks
+#       sudo nixos-rebuild switch --flake .#bottom &>nixos-switch.log || (cat nixos-switch.log | grep --color error && exit 1)
+#       # Get current generation metadata
+#       current=$(nixos-rebuild list-generations | grep current)
+#       # Commit all changes witih the generation metadata
+#       git commit -am "$current"
+#       # Back to where you were
+#       popd
+#       # Notify all OK!
+#       notify-send -e "NixOS Rebuilt OK!" --icon=software-update-available
+#     '';
+# in
+{
   imports = [
     ./hardware-configuration.nix
     ../common/desktop
@@ -208,7 +198,6 @@ in {
     nvd
     comma
     nix-health
-    nyx-rebuild
   ];
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
