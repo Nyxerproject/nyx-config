dd271f53f62748bd0f72fa8a72380df2181e0b5a
Nyxerproject <nxyerproject@gmail.com>
2024-07-20 16:05:25 -0500

    8 current   2024-07-20 16:04:37  24.11.20240719.1d9c2c9  6.6.41                          *

--- hosts/common/desktop/packages.nix
+++ hosts/common/desktop/packages.nix
@@ -1,12 +1,14 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
-    # Instant messaging packages
+    # Polkit agent
+    kdePackages.polkit-kde-agent-1
+
+    # Instant messaging packages # TODO consider moving to home. shouldn't be on every desktop and user
     webcord
     tdesktop
     element-desktop
     element-desktop-wayland
     iamb
-
     # copy paste stuff
     wl-clipboard
     # audio
