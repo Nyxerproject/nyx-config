984798579c5a8e3ba16d0e7273f3db87f5cb79a8
Nyxerproject <nxyerproject@gmail.com>
2024-07-28 22:27:27 -0500

    192 current  2024-07-28 22:27:22  24.11.20240727.b73c222  6.9.8-zen1                          *

--- hosts/common/desktop/gaming.nix
+++ hosts/common/desktop/gaming.nix
@@ -1,16 +1,28 @@
 {
   lib,
+  config,
   pkgs,
   ...
-}: {
+}: let
+  steam = pkgs.steam.override {
+    extraPkgs = pkgs:
+      with pkgs; [
+        gamescope
+      ];
+  };
+in {
   environment.systemPackages = with pkgs; [
     r2modman
     steam-run
     protontricks
+    lutris
+    gamemode
+    gamescope
   ];
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
+    gamescopeSession.enable = true; # Automatically invoke gamescope
     dedicatedServer.openFirewall = false;
     localNetworkGameTransfers.openFirewall = true;
     extraCompatPackages = with pkgs; [
@@ -47,40 +59,36 @@
       }))
     ];
   };
-  programs.gamemode.enable = true;
-  nixpkgs.config.allowUnfreePredicate = pkg:
-    builtins.elem (lib.getName pkg) [
-      "steam"
-      "steam-original"
-      "steam-runtime"
+  # nixpkgs.config.allowUnfreePredicate = pkg:
+  #   builtins.elem (lib.getName pkg) [
+  #     "steam"
+  #     "steam-original"
+  #     "steam-runtime"
+  #   ];
+
+  programs.gamescope = {
+    enable = true;
+    capSysNice = true;
+    args = [
+      "-f"
     ];
+  };
 
-  # extraCompatPackages = with pkgs; [
-  # proton-ge-bin
-  # (proton-ge-bin.overrideAttrs (finalAttrs: _: {
-  # version = "GE-Proton9-4-rtsp7";
-  # src = pkgs.fetchzip {
-  # url = "https://github.com/SpookySkeletons/proton-ge-rtsp/releases/download/${finalAttrs.version}/${finalAttrs.version}.tar.gz";
-  # hash = "sha256-l/zt/Kv6g1ZrAzcxDNENByHfUp/fce3jOHVAORc5oy0=";
-  # };
-  # }))
-  # ];
-
-  # programs.gamemode = {
-  # enable = true;
-  # settings = {
-  # general = {
-  # defaultgov = config.powerManagement.cpuFreqGovernor;
-  # desiredgov = "performance";
-  # softrealtime = "on";
-  # renice = 10;
-  # ioprio = 1;
-  # inhibit_screensaver = 0;
-  # };
-  # custom = {
-  # start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
-  # stop = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
-  # };
-  # };
-  # };
+  programs.gamemode = {
+    enable = true;
+    settings = {
+      general = {
+        defaultgov = config.powerManagement.cpuFreqGovernor;
+        desiredgov = "performance";
+        # softrealtime = "on";
+        # renice = 10;
+        # ioprio = 1;
+        inhibit_screensaver = 0;
+      };
+      custom = {
+        start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
+        stop = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
+      };
+    };
+  };
 }
