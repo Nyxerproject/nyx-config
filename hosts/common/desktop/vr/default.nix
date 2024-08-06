2fc411e5e3cca68206249bea280f070c97f5d668
Nyxerproject <nxyerproject@gmail.com>
2024-08-05 21:23:27 -0500

    213 current  2024-08-05 21:23:21  24.11.20240802.d049530  6.10.1-zen1                          *

--- hosts/common/desktop/vr/default.nix
+++ hosts/common/desktop/vr/default.nix
@@ -1,22 +1,26 @@
 {
+  config,
   inputs,
   pkgs,
   ...
-}: {
-  # imports = [
-  # inputs.nixpkgs-xr.nixosModules.nixpkgs-xr
-  # ];
-
-  programs.envision.enable = true;
-
+}: let
+  monado = pkgs.monado.overrideAttrs (previousAttrs: {
+    src = previousAttrs.src.override {
+      rev = "9baa28ee235ba0af9672650363de0eb86db6f646";
+      sha256 = "sha256-9+iGEc40xNzmQrjr+80PHpAN9yrz3ohTtZpNu1y5dE8=";
+    };
+  });
+in {
   environment.variables = {
     LH_DEFAULT_BRIGHTNESS = 1;
     STEAMVR_LH_ENABLE = "1";
     XRT_COMPOSITOR_COMPUTE = "1";
     WMR_HANDTRACKING = "0";
     LIBMONADO_PATH = "${pkgs.monado}/lib/libmonado.so";
+    U_PACING_APP_IMMEDIATE_WAIT_FRAME_RETURN = "1";
   };
 
+  programs.envision.enable = true;
   services.monado = {
     enable = true;
     defaultRuntime = true;
