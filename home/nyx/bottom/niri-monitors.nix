2a84353fb8a6180b207c8c2212fa2b2cb943b329
Nyxerproject <nxyerproject@gmail.com>
2024-08-12 18:46:19 -0500

    222 current  2024-08-12 18:46:12  24.11.20240804.cb9a96f  6.10.1-zen1                          *

--- home/nyx/bottom/niri-monitors.nix
+++ home/nyx/bottom/niri-monitors.nix
@@ -18,37 +18,37 @@
     # anki-bin
   ];
   programs.niri.settings = {
-    outputs."DP-1" = {
-      mode = {
-        height = 1200;
-        width = 1920;
-        refresh = 60.0;
-      };
-      transform.rotation = 90;
-      position = {
-        x = 1920;
-        y = -540;
-      };
-    };
+    # outputs."DP-1" = {
+    #   mode = {
+    #     height = 1200;
+    #     width = 1920;
+    #     refresh = 60.0;
+    #   };
+    #   transform.rotation = 90;
+    #   position = {
+    #     x = 1920;
+    #     y = -540;
+    #   };
+    # };
 
-    outputs."DP-2" = {
-      mode = {
-        height = 2880;
-        width = 1600;
-        refresh = 144.000;
-      };
-    };
+    # outputs."DP-2" = {
+    #   mode = {
+    #     height = 2880;
+    #     width = 1600;
+    #     refresh = 144.000;
+    #   };
+    # };
 
-    outputs."DP-3" = {
-      mode = {
-        height = 1080;
-        width = 1920;
-        refresh = 144.01;
-      };
-      position = {
-        x = 0;
-        y = 0;
-      };
-    };
+    # outputs."DP-3" = {
+    #   mode = {
+    #     height = 1080;
+    #     width = 1920;
+    #     refresh = 144.01;
+    #   };
+    #   position = {
+    #     x = 0;
+    #     y = 0;
+    #   };
+    # };
   };
 }
