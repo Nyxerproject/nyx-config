1728642197d0163e38510f43c6fe59ff35a1cb06
Nyxerproject <nxyerproject@gmail.com>
2024-08-29 20:09:20 -0600

    256 current  2024-08-29 20:09:12  24.11.20240826.271ffd0  6.10.6-cachyos                          *

--- hosts/bottom/configuration.nix
+++ hosts/bottom/configuration.nix
@@ -31,7 +31,7 @@
     };
   };
   chaotic.scx.enable = true; # by default uses scx_rustland scheduler
-  chaotic.scx.scheduler = "scx_central";
+  chaotic.scx.scheduler = "scx_flatcg";
 
   services = {
     desktopManager.plasma6.enable = true;
