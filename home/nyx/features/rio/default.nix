5198913077e9382be9521d3a1e7980f9422a3610
nyxerproject <nxyerproject@gmail.com>
2024-11-24 19:51:31 -0700

    88 current  2024-11-24 19:51:26  25.05.20241119.23e89b7  6.6.62                          *

--- home/nyx/features/rio/default.nix
+++ home/nyx/features/rio/default.nix
@@ -7,14 +7,34 @@
       #theme = "default";
       window = {
         foreground-opacity = 1.0;
-        background-opacity = 0.96;
+        background-opacity = 0.6;
         blur = true;
         decorations = "Disabled";
       };
+      # fonts = {
+      #   family = "FiraCode Nerd Font";
+      #   #size = 15;
+      # };
       fonts = {
-        family = "FiraCode Nerd Font";
-        #size = 15;
+        size = 10;
+        normal = {
+          family = "FiraCode Nerd Font";
+          style = "Regular";
+        };
+        bold = {
+          family = "FiraCode Nerd Font";
+          style = "Bold";
+        };
+        italic = {
+          family = "FiraCode Nerd Font";
+          style = "Italic";
+        };
+        bold_italic = {
+          family = "FiraCode Nerd Font";
+          style = "Bold_Italic";
+        };
       };
+
       # shell = {
       #   program = "${pkgs.zellij}/bin/zellij";
       #   args = ["-s" "local-dev" "attach" "-c" "local-dev"];
