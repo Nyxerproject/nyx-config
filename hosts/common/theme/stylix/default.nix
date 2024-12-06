09fdc0b405ae2de8fd026b88ed56c55667f3462b
nyxerproject <nxyerproject@gmail.com>
2024-12-06 06:47:35 -0700

    feat: fix(niri): keybinds change

--- hosts/common/theme/stylix/default.nix
+++ hosts/common/theme/stylix/default.nix
@@ -11,9 +11,10 @@
     autoEnable = true;
     image = ./../backgrounds/background.png;
     polarity = "dark";
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
+    #base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
     opacity = {
-      terminal = 0.9;
+      terminal = 0.75;
       popups = 0.9;
     };
     cursor = {
