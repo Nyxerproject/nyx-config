243e39f5821d99e4629dab294c54058a59b627ac
Nyxerproject <nyxerproject@gmail.com>
2024-12-07 13:45:52 -0700

    feat(stylix): caroline theme

--- hosts/common/theme/stylix/default.nix
+++ hosts/common/theme/stylix/default.nix
@@ -12,6 +12,8 @@
     image = ./../backgrounds/background.png;
     polarity = "dark";
     base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
+    #base16Scheme = "${pkgs.base16-schemes}/share/themes/caroline.yaml";
+    #base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
     #base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
     opacity = {
       terminal = 0.85;
