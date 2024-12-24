6ad9e494fdbc7434535250bdf42fa858bd6f4835
nyxerproject <nxyerproject@gmail.com>
2024-12-23 23:18:07 -0700

    fix(proton): remove fake hash

--- hosts/common/desktop/gaming.nix
+++ hosts/common/desktop/gaming.nix
@@ -24,7 +24,7 @@
           version = "GE-Proton9-20-rtsp16";
           src = pkgs.fetchzip {
             url = "https://github.com/SpookySkeletons/proton-ge-rtsp/releases/download/${finalAttrs.version}/${finalAttrs.version}.tar.gz";
-            hash = "sha256-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx=";
+            hash = "sha256-iq7oiDW5+51wzqYwASOGSV922c/pg1k29MdkIXlT34k=";
           };
         }))
       ];
