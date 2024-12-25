bce70e098ee784e5ef6e333ce273f80cb6e93b26
nyxerproject <nxyerproject@gmail.com>
2024-12-25 13:54:54 -0700

    fix(syncthing): add gruop
    group*

--- hosts/common/services/syncthing/default.nix
+++ hosts/common/services/syncthing/default.nix
@@ -3,6 +3,7 @@
     syncthing = {
       enable = true;
       user = "nyx";
+      group = "users";
       dataDir = "/home/nyx/Documents";
       configDir = "/home/nyx/.config/syncthing";
       overrideDevices = true; # overrides any devices added or
