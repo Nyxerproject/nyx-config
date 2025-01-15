53e0e33568ffc5eba7177ad80c00ca2b0005fc75
Nyxerproject <nyxerproject@gmail.com>
2025-01-14 17:17:32 -0700

    fix: update

--- system/bottom/home.nix
+++ system/bottom/home.nix
@@ -1,7 +1,19 @@
 {
   home-manager.users.nyx = {
-    programs.niri.settings = {
-      outputs."DP-2" = {
+    programs.niri.settings.outputs = {
+      "Acer Technologies Acer XFA240 0x01826F9E" = {
+        mode = {
+          height = 1080;
+          width = 1920;
+          refresh = 144.01;
+        };
+        position = {
+          x = 0;
+          y = 0;
+        };
+      };
+
+      "Hewlett Packard HP Z24x CN44440B42" = {
         mode = {
           height = 1200;
           width = 1920;
@@ -11,17 +23,6 @@
         position = {
           x = 1920;
           y = -540;
-        };
-      };
-      outputs."DP-3" = {
-        mode = {
-          height = 1080;
-          width = 1920;
-          refresh = 144.01;
-        };
-        position = {
-          x = 0;
-          y = 0;
         };
       };
     };
