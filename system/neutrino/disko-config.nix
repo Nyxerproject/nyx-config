b718e0332047a40f070ac5b50a988020e72fc864
nyxerproject <nxyerproject@gmail.com>
2025-01-17 17:19:17 -0700

    fix: stuff

--- system/neutrino/disko-config.nix
+++ system/neutrino/disko-config.nix
@@ -7,46 +7,75 @@
         content = {
           type = "gpt";
           # Example to create a bios compatible gpt partition
+          #           partitions = {
+          #             boot = {
+          #               name = "boot";
+          #               size = "1M";
+          #               type = "EF02";
+          #             };
+          #             esp = {
+          #               name = "ESP";
+          #               size = "500M";
+          #               type = "EF00";
+          #               content = {
+          #                 type = "filesystem";
+          #                 format = "vfat";
+          #                 mountpoint = "/boot";
+          #               };
+          #             };
+          #             root = {
+          #               name = "root";
+          #               size = "100%";
+          #               content = {
+          #                 type = "lvm_pv";
+          #                 vg = "pool";
+          #               };
+          #             };
+          #           };
+          #         };
+          #       };
+          #       lvm_vg = {
+          #         pool = {
+          #           type = "lvm_vg";
+          #           lvs = {
+          #             root = {
+          #               size = "100%FREE";
+          #               content = {
+          #                 type = "filesystem";
+          #                 format = "ext4";
+          #                 mountpoint = "/";
+          #                 mountOptions = [
+          #                   "defaults"
+          #                 ];
+          #               };
+          #             };
+          #           };
+          #         };
+          #       };
+          #     };
+          #   };
+          # }
           partitions = {
-            boot = {
-              name = "boot";
-              size = "1M";
-              type = "EF02";
-            };
-            esp = {
-              name = "ESP";
-              size = "500M";
+            # boot = {
+            #   size = "1M";
+            #   type = "EF02"; # for grub MBR
+            # };
+            ESP = {
               type = "EF00";
+              size = "500M";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
+                mountOptions = ["umask=0077"];
               };
             };
             root = {
-              name = "root";
               size = "100%";
-              content = {
-                type = "lvm_pv";
-                vg = "pool";
-              };
-            };
-          };
-        };
-      };
-      lvm_vg = {
-        pool = {
-          type = "lvm_vg";
-          lvs = {
-            root = {
-              size = "100%FREE";
               content = {
                 type = "filesystem";
                 format = "ext4";
                 mountpoint = "/";
-                mountOptions = [
-                  "defaults"
-                ];
               };
             };
           };
@@ -55,33 +84,3 @@
     };
   };
 }
-#           partitions = {
-#             boot = {
-#               size = "1M";
-#               type = "EF02"; # for grub MBR
-#             };
-#             ESP = {
-#               size = "512M";
-#               type = "EF00";
-#               content = {
-#                 type = "filesystem";
-#                 format = "vfat";
-#                 mountpoint = "/boot";
-#                 mountOptions = ["umask=0077"];
-#               };
-#             };
-#             root = {
-#               size = "100%";
-#               content = {
-#                 type = "filesystem";
-#                 format = "ext4";
-#                 mountpoint = "/";
-#               };
-#             };
-#           };
-#         };
-#       };
-#     };
-#   };
-# }
-
--- users/nyx.nix
+++ users/nyx.nix
@@ -3,7 +3,7 @@
     isNormalUser = true;
     description = "nyx";
     extraGroups = ["networkmanager" "wheel"];
-    #hashedPassword = "$y$j9T$d2moNWhXMPaPXQQlBS9J7/$uQKwf.Y0xRKzbaOZCFybnrUeqB3HAnUiuzL17wA7/P3";
-    hashedPassword = config.sops.secrets.my-password.path;
+    hashedPassword = "$y$j9T$d2moNWhXMPaPXQQlBS9J7/$uQKwf.Y0xRKzbaOZCFybnrUeqB3HAnUiuzL17wA7/P3";
+    #hashedPassword = config.sops.secrets.my-password.path;
   };
 }
