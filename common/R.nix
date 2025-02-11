940010140850257558cbe4bf2dc1102eca3726dc
Nyxerproject <nyxerproject@gmail.com>
2025-02-10 18:37:25 -0700

    fix: temp. remove RStudio until pr gets merged

--- common/R.nix
+++ common/R.nix
@@ -6,9 +6,23 @@
   environment = {
     systemPackages = with pkgs; let
       # https://wiki.nixos.org/wiki/R
-      RStudio-for-stats = pkgs.rstudioWrapper.override {packages = with pkgs.rPackages; [];};
+      # RStudio-for-stats' = pkgs.rstudioWrapper.override {
+      #   packages = with pkgs.rPackages; [];
+      # };
+      RStudio-for-stats = pkgs.rstudioWrapper.overrideAttrs (oldAttrs: {
+        #RStudio-for-stats = RStudio-for-stats'.overrideAttrs (oldAttrs: {
+        patches =
+          (oldAttrs.patches or [])
+          ++ [
+            (fetchurl {
+              name = "fix.patch";
+              url = "https://github.com/NixOS/nixpkgs/commit/5d57a90e2abf555de084876009bffba7dc40f0f2.patch";
+              hash = "sha256-fHCSloBiofsXi9JtbEPVGTBBJ3ZikgWdDy5NCFZW5vY=";
+            })
+          ];
+      });
     in [
-      RStudio-for-stats
+      #RStudio-for-stats
       rPackages.rmarkdown
     ];
   };
