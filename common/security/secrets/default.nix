340c33a39d30806c0b36b5d3ee0096d9a84c0fa1
Nyxerproject <nyxerproject@gmail.com>
2025-02-14 19:14:16 -0700

    fix: remove ddclient user

--- common/security/secrets/default.nix
+++ common/security/secrets/default.nix
@@ -9,13 +9,14 @@
     age = {
       sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"]; # ssh key are assumed to exist
       # keyFile = "/nix/persist/var/lib/sops-nix/key.txt";
+      keyFile = "/home/nyx/.config/sops/age/keys.txt";
       generateKey = true;
     };
     secrets = {
       "namecheap_password" = {
         restartUnits = ["nextcloud.service"];
-        owner = "ddclient";
-        group = "ddclient";
+        # owner = "ddclient";
+        # group = "ddclient";
         path = "/var/lib/ddclient/secrets";
       };
       "nextcloud/adminpass" = {
