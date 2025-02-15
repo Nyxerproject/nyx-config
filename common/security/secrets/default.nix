278c4180ac4663ec1e987f16bd33011304181d69
Nyxerproject <nyxerproject@gmail.com>
2025-02-14 19:36:44 -0700

    refactor: remove dead code

--- common/security/secrets/default.nix
+++ common/security/secrets/default.nix
@@ -13,25 +13,6 @@
       generateKey = true;
     };
     secrets = {
-      "namecheap_password" = {
-        restartUnits = ["nextcloud.service"];
-        # owner = "ddclient";
-        # group = "ddclient";
-        path = "/var/lib/ddclient/secrets";
-      };
-      "nextcloud/adminpass" = {
-        restartUnits = ["nextcloud.service"];
-        # owner = lib.mkForce "nextcloud";
-        # group = lib.mkForce "nextcloud";
-        # mode = lib.mkForce "0440";
-        #restartUnits = ["phpfpm-nextcloud.service"];
-      };
-      home-assistant-secrets = {
-        restartUnits = ["home-assistant.service"];
-        format = "yaml";
-        # owner = "hass";
-        # path = "/var/lib/hass/secrets.yaml";
-      };
       my-password.neededForUsers = true;
     };
   };
