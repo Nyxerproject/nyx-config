c9eca72fe293c1ad422db242145218f50a755464
Nyxerproject <nyxerproject@gmail.com>
2025-02-19 12:31:00 -0700

    feat: add pandoc

---
+++ common/R.nix
@@ -0,0 +1,20 @@
+{
+  pkgs,
+  inputs,
+  ...
+}: {
+  environment = {
+    systemPackages = with pkgs; let
+      RStudio-for-stats = pkgs.rstudioWrapper.override {
+        packages = with pkgs.rPackages; [
+          #insert packages here
+          ggplot2
+        ];
+      };
+    in [
+      RStudio-for-stats
+      rPackages.rmarkdown
+      pandoc
+    ];
+  };
+}
