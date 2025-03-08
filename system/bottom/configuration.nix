394bcaffaee211086b1d7bc5bffe2e652bd6cd7d
nyxerproject <nxyerproject@gmail.com>
2025-03-08 12:44:44 -0700

    feat: blacklisted amdgpu on bottom

--- system/bottom/configuration.nix
+++ system/bottom/configuration.nix
@@ -1,20 +1,19 @@
-{pkgs, ...}: {
+{
   networking.hostName = "bottom";
-
   services = {
     scx = {
       enable = true;
       scheduler = "scx_rusty";
     };
-    displayManager.defaultSession = "niri";
+    # displayManager.defaultSession = "niri";
     xserver.enable = true;
   };
   hardware.graphics.enable = true;
   # hardware.nvidia.open = true;
   # services.xserver.videoDrivers = ["nvidia"];
   # boot.kernelParams = ["nvidia_drm.fbdev=1"];
-
-  environment.systemPackages = with pkgs; [
-    # cudaPackages.cudatoolkit-legacy-runfile # for Wivrn?
-  ];
+  boot.kernelParams = ["module_blacklist=amdgpu"];
+  # environment.systemPackages = with pkgs; [
+  # cudaPackages.cudatoolkit-legacy-runfile # for Wivrn?
+  # ];
 }
