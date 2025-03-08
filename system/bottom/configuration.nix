cd82ca0e3973677029e3098f1cf2efa60cbc4192
Nyxerproject <nxyerproject@gmail.com>
2025-03-08 12:57:22 -0700

    feat: added prop. NVIDA driver to bottom

--- system/bottom/configuration.nix
+++ system/bottom/configuration.nix
@@ -5,14 +5,18 @@
       enable = true;
       scheduler = "scx_rusty";
     };
-    # displayManager.defaultSession = "niri";
-    xserver.enable = true;
+    displayManager.defaultSession = "niri";
+    xserver = {
+      enable = true;
+      videoDrivers = ["nvidia"];
+    };
   };
   hardware.graphics.enable = true;
-  # hardware.nvidia.open = true;
-  # services.xserver.videoDrivers = ["nvidia"];
-  # boot.kernelParams = ["nvidia_drm.fbdev=1"];
-  boot.kernelParams = ["module_blacklist=amdgpu"];
+  hardware.nvidia.open = true;
+  boot.kernelParams = [
+    "nvidia_drm.fbdev=1"
+    "module_blacklist=amdgpu"
+  ];
   # environment.systemPackages = with pkgs; [
   # cudaPackages.cudatoolkit-legacy-runfile # for Wivrn?
   # ];
