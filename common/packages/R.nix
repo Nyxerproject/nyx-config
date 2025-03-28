ede24f60bf5f9bdb000bf4a3892088386c8351a3
nyxerproject <nxyerproject@gmail.com>
2025-03-27 22:47:37 -0600

    feat: added tinytex

--- common/packages/R.nix
+++ common/packages/R.nix
@@ -1,7 +1,12 @@
 {pkgs, ...}: {
   environment = {
     systemPackages = with pkgs; let
-      RStudio-for-stats = pkgs.rstudioWrapper.override {packages = [pkgs.rPackages.ggplot2];};
+      RStudio-for-stats = pkgs.rstudioWrapper.override {
+        packages = with pkgs; [
+          rPackages.ggplot2
+          rPackages.tinytex
+        ];
+      };
     in [
       RStudio-for-stats
       rPackages.rmarkdown
