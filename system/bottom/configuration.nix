b5c61ec4cad762f9da0bf3d171cce1e4f7373f4e
Nyxerproject <nxyerproject@gmail.com>
2025-04-05 00:08:07 -0600

    fix: nvidia stuff for bottom

--- system/bottom/configuration.nix
+++ system/bottom/configuration.nix
@@ -9,16 +9,13 @@
     displayManager.defaultSession = "niri";
     xserver = {
       enable = true;
-      # videoDrivers = ["nvidia"];
+      videoDrivers = ["nvidia"];
     };
   };
   hardware.graphics.enable = true;
-  # hardware.nvidia.open = true;
-  # boot.kernelParams = [
-  #   "nvidia_drm.fbdev=1"
-  #   "module_blacklist=amdgpu"
-  # ];
-  # environment.systemPackages = with pkgs; [
-  # cudaPackages.cudatoolkit-legacy-runfile # for Wivrn?
-  # ];
+  hardware.nvidia.open = true;
+  boot.kernelParams = [
+    "nvidia_drm.fbdev=1"
+    "module_blacklist=amdgpu"
+  ];
 }
