c13b709f066aae4ff8dcbac244c923b50b7b77a7
nyxerproject <nxyerproject@gmail.com>
2025-04-22 18:29:27 -0600

    fix: typo

--- system/down/configuration.nix
+++ system/down/configuration.nix
@@ -1,11 +1,10 @@
 {pkgs, ...}: {
   networking.hostName = "down";
   boot.kernelPackages = pkgs.linuxPackages_cachyos;
-  services = {
-    scx = {
-      enable = true;
-      scheduler = "scx_lavd";
-    };
+  services.scx = {
+    enable = true;
+    scheduler = "scx_lavd";
+  };
   boot.loader.grub.device = "/dev/disk/by-id/nvme-WDC_PC_SN730_SDBPNTY-512G-1006_20204F801215_1";
   services.xserver.enable = true;
   hardware.graphics = {
