f4ebf5a06556cde6dba2b2245d3da3fce0c50387
nyxerproject <nxyerproject@gmail.com>
2025-05-16 08:16:30 -0600

    feat: added stuff

--- common/packages/R.nix
+++ common/packages/R.nix
@@ -1,18 +1,16 @@
 {pkgs, ...}: {
-  environment = {
-    systemPackages = with pkgs; let
-      RStudio-for-stats = pkgs.rstudioWrapper.override {
-        packages = with pkgs; [
-          rPackages.ggplot2
-          rPackages.tinytex
-          rPackages.rmarkdown
-        ];
-      };
-    in [
-      RStudio-for-stats
-      tectonic # latex compiler
-      texliveFull
-      pandoc
-    ];
-  };
+  environment.systemPackages = with pkgs; let
+    RStudio-for-stats = pkgs.rstudioWrapper.override {
+      packages = with pkgs; [
+        rPackages.ggplot2
+        rPackages.tinytex
+        rPackages.rmarkdown
+      ];
+    };
+  in [
+    RStudio-for-stats
+    tectonic # latex compiler
+    texliveFull
+    pandoc
+  ];
 }
--- common/packages/default.nix
+++ common/packages/default.nix
@@ -1,4 +1,32 @@
-{
-  imports = [./scripts.nix ./cli];
+{pkgs, ...}: let
+in {
+  imports = [
+    ./scripts.nix
+    ./cli
+    # ./mistralrs.nix
+  ];
+  environment.systemPackages = with pkgs; [
+    (mistral-rs.overrideAttrs (finalAttrs: previousAttrs: {
+      version = "unstable-2025-05-16";
+      src = pkgs.fetchFromGitHub {
+        owner = "EricLBuehler";
+        repo = "mistral.rs";
+        rev = "380da230bd7cff64e5d22b2876f5841b662fba3b";
+        hash = "sha256-fwpVmdaPOeHx+Jmz8CZGkRuWozflAjxLQibOd1JqTAE=";
+      };
+      # Doesn't require IFD
+      # cargoDeps = previousAttrs.cargoDeps.overrideAttrs {
+      #   name = "nil-vendor.tar.gz";
+      #   inherit (finalAttrs) src;
+      #   outputHash = pkgs.lib.fakeHash;
+      # };
+      # Requires IFD
+      cargoDeps = pkgs.rustPlatform.importCargoLock {
+        lockFile = finalAttrs.src + "/Cargo.lock";
+        allowBuiltinFetchGit = true;
+      };
+      cargoHash = null;
+    }))
+  ];
   # GUI stuff is imported via '/common/desktop/default.nix'
 }
